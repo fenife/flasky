1dc4e8c9c5f377aed2f3ee907ca86e39572b5570
feng <2209956642@qq.com>
2017-08-02 16:21:23 +0800

    使用Flask-WTF处理Web表单

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 """
 hello.py
 
-使用Flask-Moment本地化日期和时间
+使用Flask-WTF处理Web表单
 """
 
 from datetime import datetime
@@ -9,9 +9,14 @@ from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 # 初始化
 app = Flask(__name__)
+# 设置Flask-WTF
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 # 使用扩展模块Flask-Script支持命令行选项
 manager = Manager(app)
@@ -22,21 +27,27 @@ bootstrap = Bootstrap(app)
 # 初始化Flask-Moment
 moment = Moment(app)
 
+# 定义表单类
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
 # 错误处理
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
-
-@app.errorhandler(500)
-def internal_server_error(e):
     return render_template('500.html'), 500
 
 # URL和视图函数的映射关系（路由）
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     """ 程序URL根地址'/'的响应函数 """
-    return render_template('index.html',
-            current_time=datetime.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.date = ''     # 清空表单数据
+    return render_template('index.html', form=form, name=name)
 
 # 支持URL的动态参数
 @app.route('/user/<name>')
