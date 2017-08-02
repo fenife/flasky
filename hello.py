9cf017bba45384a02dca51376bba0c9677ea3f7f
feng <2209956642@qq.com>
2017-08-02 16:45:32 +0800

    重定向和用户会话

--- hello.py
+++ hello.py
@@ -1,11 +1,10 @@
 """
 hello.py
 
-使用Flask-WTF处理Web表单
+重定向和用户会话
 """
 
-from datetime import datetime
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -27,32 +26,33 @@ bootstrap = Bootstrap(app)
 # 初始化Flask-Moment
 moment = Moment(app)
 
+
 # 定义表单类
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[Required()])
     submit = SubmitField('Submit')
 
+
 # 错误处理
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
     return render_template('500.html'), 500
+
 
 # URL和视图函数的映射关系（路由）
 @app.route('/', methods=['GET', 'POST'])
 def index():
     """ 程序URL根地址'/'的响应函数 """
-    name = None
+
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.date = ''     # 清空表单数据
-    return render_template('index.html', form=form, name=name)
-
-# 支持URL的动态参数
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))   # 重定向URL
+    return render_template('index.html', form=form, name=session.get('name'))
 
 
 # 启动服务器
