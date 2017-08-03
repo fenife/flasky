1dd45079f4d1a56bc404e662b4316671e7512cca
feng <2209956642@qq.com>
2017-08-03 13:48:56 +0800

    创建认证蓝本

--- app/__init__.py
+++ app/__init__.py
@@ -21,8 +21,10 @@ def create_app(config_name):
     db.init_app(app)
 
     # 附加路由和自定义的错误页面
-
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
