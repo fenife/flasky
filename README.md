36cd46c7ad7066e9dc966565fd24f737f821d421
feng <2209956642@qq.com>
2017-08-04 10:46:22 +0800

    确认账户邮件

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 
 # 说明
 本实例代码参考自[Flask Web Development](https://github.com/miguelgrinberg/flasky)
+和[Flask中文文档](http://docs.jinkan.org/docs/flask/index.html)
--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
