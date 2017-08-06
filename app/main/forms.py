6022f26c500e598b389acff831f0ecfa1f8b79a2
feng <2209956642@qq.com>
2017-08-06 14:16:32 +0800

    修改为中文

--- app/main/forms.py
+++ app/main/forms.py
@@ -8,30 +8,30 @@ from ..models import Role, User
 
 
 class NameForm(FlaskForm):
-    name = StringField('What is your name?', validators=[Required()])
-    submit = SubmitField('Submit')
+    name = StringField('你的名字？', validators=[Required()])
+    submit = SubmitField('提交')
 
 
 class EditProfileForm(FlaskForm):
-    name = StringField('Real name', validators=[Length(0, 64)])
-    location = StringField('Location', validators=[Length(0, 64)])
-    about_me = TextAreaField('About me')
-    submit = SubmitField('Submit')
+    name = StringField('真实名字', validators=[Length(0, 64)])
+    location = StringField('位置', validators=[Length(0, 64)])
+    about_me = TextAreaField('个人简介')
+    submit = SubmitField('提交')
 
 
 class EditProfileAdminForm(FlaskForm):
-    email = StringField('Email', validators=[Required(), Length(1, 64),
+    email = StringField('邮箱', validators=[Required(), Length(1, 64),
                                              Email()])
-    username = StringField('Username', validators=[
+    username = StringField('用户名', validators=[
         Required(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
                                           'Usernames must have only letters, '
                                           'numbers, dots or underscores')])
     confirmed = BooleanField('Confirmed')
-    role = SelectField('Role', coerce=int)
-    name = StringField('Real name', validators=[Length(0, 64)])
-    location = StringField('Location', validators=[Length(0, 64)])
-    about_me = TextAreaField('About me')
-    submit = SubmitField('Submit')
+    role = SelectField('角色', coerce=int)
+    name = StringField('真实名字', validators=[Length(0, 64)])
+    location = StringField('位置', validators=[Length(0, 64)])
+    about_me = TextAreaField('个人简介')
+    submit = SubmitField('提交')
 
     def __init__(self, user, *args, **kwargs):
         super(EditProfileAdminForm, self).__init__(*args, **kwargs)
@@ -42,19 +42,19 @@ class EditProfileAdminForm(FlaskForm):
     def validate_email(self, field):
         if field.data != self.user.email and \
                 User.query.filter_by(email=field.data).first():
-            raise ValidationError('Email already registered.')
+            raise ValidationError('此邮箱已注册')
 
     def validate_username(self, field):
         if field.data != self.user.username and \
                 User.query.filter_by(username=field.data).first():
-            raise ValidationError('Username already in use.')
+            raise ValidationError('此用户名已被使用')
 
 
 class PostForm(FlaskForm):
-    body = PageDownField("What's on your mind?", validators=[Required()])
-    submit = SubmitField('Submit')
+    body = PageDownField("创建新的主题", validators=[Required()])
+    submit = SubmitField('提交')
 
 
 class CommentForm(FlaskForm):
-    body = StringField('Enter your comment', validators=[Required()])
-    submit = SubmitField('Submit')
+    body = StringField('输入评论', validators=[Required()])
+    submit = SubmitField('提交')
