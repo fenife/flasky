aefe3ef12e9e637a88cb97e83789a0aca49c7b8d
feng <2209956642@qq.com>
2017-08-06 11:29:02 +0800

    邮件认证

--- app/auth/views.py
+++ app/auth/views.py
@@ -13,18 +13,11 @@ from .forms import LoginForm, RegistrationForm, ChangePasswordForm,\
 def before_request():
     if current_user.is_authenticated:
         current_user.ping()
-        pass
-
-# 邮件未认证时，暂时不作处理
-'''
-    if current_user.is_authenticated:
-        current_user.ping()
         if not current_user.confirmed \
                 and request.endpoint \
                 and request.endpoint[:5] != 'auth.' \
                 and request.endpoint != 'static':
             return redirect(url_for('auth.unconfirmed'))
-'''
 
 
 @auth.route('/unconfirmed')
